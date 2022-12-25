c85ab28a8bc8c4d6df936c8054b006ffb61d5177
caijianhaoa <1784581164@qq.com>
2022-12-26 01:05:23 +0800

    feat: fromEvent

--- test/index.js
+++ test/index.js
@@ -1,24 +1,31 @@
-import { Observable, of, from } from '../lib/index.js'
+import { Observable, of, from, fromEvent } from '../lib/index.js'
 // import { of, from } from 'rxjs'
 
-const promiseLikeObservable = from(Promise.resolve(4))
+// const promiseLikeObservable = from(Promise.resolve(4))
 
-promiseLikeObservable.subscribe({
-  next: (value) => {
-    console.log(value)
-  },
-  complete: () => {
-    console.log('complete')
-  },
-})
+// promiseLikeObservable.subscribe({
+//   next: (value) => {
+//     console.log(value)
+//   },
+//   complete: () => {
+//     console.log('complete')
+//   },
+// })
 
-const arrayLikeObservable = of(1, 2, 3)
+// const arrayLikeObservable = of(1, 2, 3)
 
-arrayLikeObservable.subscribe({
-  next: (value) => {
-    console.log(value)
-  },
-  complete: () => {
-    console.log('complete')
-  },
-})
+// arrayLikeObservable.subscribe({
+//   next: (value) => {
+//     console.log(value)
+//   },
+//   complete: () => {
+//     console.log('complete')
+//   },
+// })
+
+const eventObservable = fromEvent(document, 'click')
+const subscriber = eventObservable.subscribe(console.log)
+
+setTimeout(() => {
+  subscriber.unsubscribe()
+}, 3000)
